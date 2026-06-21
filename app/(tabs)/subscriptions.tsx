9eddcff65a5a1dbc702f55adc3ccd546968d39cf
YebasMardo <yebasmardo@gmail.com>
2026-06-21 15:09:45 +0100

    Upcoming sub ui

---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView as RNSafeAreaView} from 'react-native-safe-area-context'
+import { styled } from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView)
+
+const Subscriptions = () => {
+  return (
+    <SafeAreaView className='bg-background flex-1 p-5'>
+      <Text>Subscriptions</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Subscriptions
